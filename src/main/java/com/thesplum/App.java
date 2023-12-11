c73cede0758f45c1ea0ef8f576463e4a5c377ff2
ThesplumCoder <andersonYacVpers002360@outlook.com>
2023-12-11 15:53:38 -0500

    Uso de la plantilla proporcionada por Azure Portal.

--- src/main/java/com/thesplum/App.java
+++ src/main/java/com/thesplum/App.java
@@ -1,13 +1,34 @@
 package com.thesplum;
 
+import com.azure.communication.email.models.*;
+import com.azure.communication.email.*;
+import com.azure.core.util.polling.PollResponse;
+import com.azure.core.util.polling.SyncPoller;
+
 /**
- * Hello world!
+ * Clase que contiene el punto de entrada de la aplicación.
  *
+ * @author Anderson Acuña (ThesplumCoder).
+ * @version 1.0
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+  public static void main(String[] args) {
+    final String CONNECTION_STRING = "<connection-string>";
+
+    EmailClient emailClient = new EmailClientBuilder()
+            .connectionString(CONNECTION_STRING)
+            .buildClient();
+
+    EmailAddress destination = new EmailAddress("<email-target>");
+
+    EmailMessage message = new EmailMessage()
+            .setSenderAddress("<email-domain>")
+            .setToRecipients(destination)
+            .setSubject("Message from Azure Email Service")
+            .setBodyPlainText("Hello from server.");
+
+    SyncPoller<EmailSendResult, EmailSendResult> poller = emailClient.beginSend(message, null);
+    PollResponse<EmailSendResult> result = poller.waitForCompletion();
+  }
 }
